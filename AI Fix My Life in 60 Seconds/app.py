2e4708e6c51a299214319db3a616378ec0498c9e
fuzail-developer <fuzailshaik42@gmail.com>
2026-02-25 08:28:41 +0530

    Update app.py

--- AI Fix My Life in 60 Seconds/app.py
+++ AI Fix My Life in 60 Seconds/app.py
@@ -502,6 +502,8 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     with app.app_context():
         db.create_all()
-    app.run(host="0.0.0.0", port=port, debug=False)5000)
+    app.run(host="0.0.0.0", port=port, debug=False)
+
+
 
 
